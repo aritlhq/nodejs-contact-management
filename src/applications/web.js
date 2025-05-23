d1aeb747c27766430526736ded807ab626f56109
aritlhq <aritlhq@gmail.com>
2025-05-23 22:32:45 +0700

    feat: add userRouter to web application

--- src/applications/web.js
+++ src/applications/web.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { publicRouter } from "../routers/public-api.js";
 import { errorMiddleware } from "../middleware/error-middleware.js";
+import { userRouter } from "../routers/api.js";
 
 export const web = express();
 
 web.use(express.json());
 web.use(publicRouter);
+web.use(userRouter);
 web.use(errorMiddleware);
