c12d476191df927198c95a672c2e221dff6bf6a1
aritlhq <aritlhq@gmail.com>
2025-05-23 21:42:11 +0700

    test: add utility functions for test user management

---
+++ tests/test-util.js
@@ -0,0 +1,21 @@
+import { prisma } from "../src/applications/database.js"
+import bcrypt from "bcrypt"
+
+export const removeTestUser = async () => {
+    await prisma.user.deleteMany({
+        where: {
+            username: "aristwn"
+        }
+    });
+}
+
+export const createTestUser = async () => {
+    await prisma.user.create({
+        data: {
+            username: "aristwn",
+            password: await bcrypt.hash("rahasia", 10),
+            name: "Ari Setiawan",
+            token: "123456789"
+        }
+    })
+}
