23a24b02986e8ef9c51cf6111357c2804d35ea59
aritlhq <aritlhq@gmail.com>
2025-05-25 23:44:18 +0700

    feat(router): add contact API endpoint to user router

--- src/routers/api.js
+++ src/routers/api.js
@@ -1,12 +1,17 @@
 import express from "express";
 import userController from "../controllers/user-controller.js";
 import { authMiddleware } from "../middleware/auth-middleware.js";
+import contactController from "../controllers/contact-controller.js";
 
 const userRouter = new express.Router();
 
+// User API
 userRouter.use(authMiddleware);
 userRouter.get("/api/users/current", userController.get);
 userRouter.patch("/api/users/current", userController.update);
 userRouter.delete("/api/users/logout", userController.logout);
+
+// Contact API
+userRouter.post("/api/contacts", contactController.create);
 
 export { userRouter };
