4dc8061d13c6a95aa25b293b23f7c014caac9607
aritlhq <aritlhq@gmail.com>
2025-05-25 23:44:42 +0700

    feat(validations): add contact validation schema using Joi

---
+++ src/validations/contact-validation.js
@@ -0,0 +1,12 @@
+import Joi from "joi";
+
+const createContactValidation = Joi.object({
+    first_name: Joi.string().max(100).required(),
+    last_name: Joi.string().max(100).required(),
+    phone: Joi.string().max(20).required(),
+    email: Joi.string().max(100).required(),
+});
+
+export {
+    createContactValidation
+}
