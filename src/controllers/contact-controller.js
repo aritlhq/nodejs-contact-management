32fe330aab0209fb284cf77df1965ca6566831e3
aritlhq <aritlhq@gmail.com>
2025-05-26 19:42:18 +0700

    refactor(contact-controller): remove unused remove function

--- src/controllers/contact-controller.js
+++ src/controllers/contact-controller.js
@@ -42,23 +42,8 @@ const update = async (req, res, next) => {
     }
 }
 
-const remove = async (req, res, next) => {
-    try {
-        const user = req.user;
-        const contactId = req.params.contactId;
-
-        await contactService.remove(user, contactId);
-        res.status(200).json({
-            message: "OK",
-        });
-    } catch (e) {
-        next(e);
-    }
-}
-
 export default {
     create,
     get,
-    update,
-    remove
+    update
 }
